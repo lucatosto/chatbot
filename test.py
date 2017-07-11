e827d85bb2f8f2fe569911ed557e7527f3d1250d
Luca Tosto <lucatosto1391@gmail.com>
2017-07-11 10:23:47 +0200

    prepare to merge

--- test.py
+++ test.py
@@ -3,25 +3,29 @@ import string
 from vector import vector
 from gensim.models import Word2Vec
 from CornellData import CornellData
-import os
-import ast
-import re
 import gensim
 import numpy as np
 import torch
-import copy_example
 
 print("ChatBot run!")
 #load model of gensim google vector
 #model = gensim.models.KeyedVectors.load_word2vec_format('/media/daniele/AF56-12AA/GoogleNews-vectors-negative300.bin', binary=True)
 model = gensim.models.KeyedVectors.load_word2vec_format('./GoogleNews-vectors-negative300.bin', binary=True)
+
+
 model2 = torch.load('checkpoint-2.pth')
+#insert into for structure
+output = model2[:len(model2)-1]
+
 print(model2)
+
+output = model.most_similar(positive = 'hello', topn = 1)
+
+output = output.data()
+output = output.numpy
+
+print(output)
 #load model defined in class copy_example
-
-#torch.load({'model_options': model_options, 'model_state': model.state_dict()}, './checkpoint-2.pth')
-
-#torch.save({'model_options': model_options, 'model_state': model.state_dict()}, checkpoint_path)
 
 #model_options = {'input_size': train_dataset[0][0].size(1), 'sos_idx': sos_idx, 'eos_idx': eos_idx, 'encoder_layers': opt.encoder_layers, 'lstm_size': opt.lstm_size}
 
@@ -30,12 +34,12 @@ print(model2)
 try:
     while True:
         domanda = input("You: ")
+        #pseudo code
+        #domandavettorizzata = model[domanda]
 
-        domandavettorizzata = model[domanda]
-
-        rispostavettorizzata = np.array( domandavettorizzata , dtype='f')
+        #rispostavettorizzata = np.array( domandavettorizzata , dtype='f')
         #risposta = model2.most_similar( [ rispostavettorizzata ], [], 1)
 
-        print("ChatBot: " +risposta[0])
+        print("ChatBot: " +domanda)
 except KeyboardInterrupt:
     print('\nBye bye!')
