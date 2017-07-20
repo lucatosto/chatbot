0abdb8cd28ed3c304e166b694f697f1f45f16166
dany3.91@hotmail.it <dany3.91@hotmail.it>
2017-07-20 12:54:22 +0200

    update

--- Model1.py
+++ Model1.py
@@ -105,7 +105,7 @@ class Model1(nn.Module):
                 o2=o2[0:300]
                 #print(o2.shape)
                 #o2=model.similar_by_vector(o2, topn=1)[0][0]
-                o3 = model.most_similar(positive=[o2], topn=1)[0][0]
+                o3 = model.similar_by_vector(positive=[o2], topn=1)[0][0]
                 o3 = model[o3] #prende la parola codificata dal modello
                 # Compute log-softmax
                 #o2 = F.log_softmax(o2)
